239e14fd03573440083be1add57c59d0a3de3549
Fernando Otero <f.o.otero@gmail.com>
2022-08-16 00:03:06 -0300

    Primer Commit

---
+++ MiPrimerMVTApp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Libros(models.Model):
+    nombre = models.CharField(max_length=50)
+    cantidadHojas = models.IntegerField()
+
+
+class CargarLibros(models.Model):
+    nombre = models.CharField(max_length=50)
+    cantidadHojas = models.IntegerField()
+
+
+class Autores(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+
+
+class Editoriales(models.Model):
+    nombre = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    email = models.EmailField()
+
+
+class Clasificaciones(models.Model):
+    tipo = models.CharField(max_length=50)
---
+++ MiPrimerMVTApp/views.py
@@ -0,0 +1,49 @@
+# Create your views here.
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.template import Template, Context, loader
+from MiPrimerMVTApp.models import CargarLibros, Libros
+
+
+def libros(request, nombre, cantidadHojas):
+    with open("Template/templateLibros.html", "r") as f:
+        plantilla = Template(f.read())
+        variables = {
+        "NombreLibro": nombre,
+        "CantidadHojas": cantidadHojas
+        }
+    contexto = Context(variables)
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+
+def CargarLibros(request, nombre, cantidadHojas):
+    lib = CargarLibros(nombre=nombre, cantidadHojas=cantidadHojas)
+    lib.save()
+    plantilla = loader.get_template('templateLibros.html')
+    contexto = {
+        "NombreLibro": lib.nombre,
+        "CantidadHojas": lib.cantidadHojas
+        }
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
+
+
+def mi_nombre(request,nombre):
+    text_response = f"El nombre ingresado es {nombre}"
+    return HttpResponse(text_response)
+
+def probando_template(request,nombre):
+    with open("Template/template1.html","r") as f:
+        plantilla = Template(f.read())
+        variables = {
+            "dato1":"hola mundo",
+            "dato2":"Fernando",
+            "dato3":datetime.now(),
+            "dato4":nombre,
+            "lista":[1,2,3,4]
+        }
+    contexto = Context(variables)
+    documento = plantilla.render(contexto)
+    return HttpResponse(documento)
