9abae2949fbdfa3c4aaf8cfbde8f32a4ebb4165e
Fernando Otero <f.o.otero@gmail.com>
2022-08-16 12:52:59 -0300

    Separa el archivo de URLS

--- MiPrimerMVT/urls.py
+++ MiPrimerMVT/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-
-from MiPrimerMVTApp.views import cargar_libros, cargar_autores, cargar_editoriales, cargar_clasificaciones
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('CargaLibros/<nombre>/<cantidadHojas>', cargar_libros),
-    path('CargaAutores/<nombre>/<apellido>', cargar_autores),
-    path('CargaEditoriales/<nombre>/<direccion>/<email>', cargar_editoriales),
-    path('CargaClasificaciones/<tipo>', cargar_clasificaciones),
+    path('admin', admin.site.urls),
+    path('MiPrimerMVT/', include('MiPrimerMVTApp.urls'))
 ]
