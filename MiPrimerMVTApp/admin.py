7933d67b7daf9796aac8913d41a03632522bc207
Fernando Otero <f.o.otero@gmail.com>
2022-08-17 00:30:16 -0300

    Se agregaron validaciones para campos vacios

--- MiPrimerMVTApp/admin.py
+++ MiPrimerMVTApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from MiPrimerMVTApp.models import Libros, Autores
+
+admin.site.register(Libros)
+admin.site.register(Autores)
--- MiPrimerMVTApp/urls.py
+++ MiPrimerMVTApp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from MiPrimerMVTApp.views import cargar_libros, cargar_autores, cargar_editoriales, cargar_clasificaciones, Main
 
 urlpatterns = [
-    path('',Main),
-    path('CargaLibros/<nombre>/<cantidadHojas>', cargar_libros),
-    path('CargaAutores/<nombre>/<apellido>', cargar_autores),
-    path('CargaEditoriales/<nombre>/<direccion>/<email>', cargar_editoriales),
-    path('CargaClasificaciones/<tipo>', cargar_clasificaciones),
+    path('',Main, name='MiPrimerMVTAppMain'),
+    path('CargaLibros/<nombre>/<cantidadHojas>', cargar_libros, name='MiPrimerMVTAppCargarLibros'),
+    path('CargaAutores/<nombre>/<apellido>', cargar_autores, name='MiPrimerMVTAppCargarAutores'),
+    path('CargaEditoriales/<nombre>/<direccion>/<email>', cargar_editoriales, name='MiPrimerMVTAppCargarEditoriales'),
+    path('CargaClasificaciones/<tipo>', cargar_clasificaciones, name='MiPrimerMVTAppCargarClasificaciones'),
 ]
